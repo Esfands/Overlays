984d8e4446a09e8066a1ec654658da495af95768
Adam Davies <acdavies001@gmail.com>
2022-02-11 04:42:52 -0600

    Cleanup initial setup

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Prediction {
+  event: string;
+  id: string;
+  outcome: object;
+  status: string;
+  title: string;
+}
--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/components/App';
 
 test('renders learn react link', () => {
   render(<App />);

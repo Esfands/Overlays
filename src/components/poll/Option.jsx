ebcfe9d0b8b6ecd7cf03392c9c82414977e51406
Adam Davies <acdavies001@gmail.com>
2022-02-15 05:04:52 -0600

    Fix numbers, mark winner

--- src/components/poll/Option.jsx
+++ src/components/poll/Option.jsx
@@ -1,17 +1,23 @@
+import Icons from '../Icons';
+
 const Option = ({ data, totalVotes, isWinner }) => {
-  const percentage = Math.round((data.votes / totalVotes) * 100);
+  const percentage =
+    data.votes && totalVotes ? Math.round((data.votes / totalVotes) * 100) : 0;
 
   return (
     <div className="option position-relative">
-      <div
-        className="option-fill position-absolute"
-        style={{ width: `${percentage}%` }}
-      />
-      <div className="option-text position-relative d-flex justify-content-between">
-        <span className="w-75 text-truncate">{data.title}</span>
-        <span>
-          {`${percentage}%`} ({data.votes})
-        </span>
+      {isWinner && <Icons.Trophy className="position-absolute" />}
+      <div className="option-content position-relative">
+        <div
+          className="option-fill position-absolute"
+          style={{ width: `${percentage}%` }}
+        />
+        <div className="option-text position-relative d-flex justify-content-between">
+          <span className="w-75 text-truncate">{data.title}</span>
+          <span>
+            {`${percentage}%`} ({data.votes || 0})
+          </span>
+        </div>
       </div>
     </div>
   );

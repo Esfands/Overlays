9481065b86112a52c069a63139fac37e47b4b127
Adam Davies <acdavies001@gmail.com>
2022-02-15 20:13:58 -0600

    Add extra logging to server

--- test/server/websocket.js
+++ test/server/websocket.js
@@ -10,9 +10,13 @@ module.exports = (expressServer, connections) => {
   });
 
   websocketServer.on('connection', (connection) => {
+    console.log('WebSocket connection opened');
+
     connections.push(connection);
 
     connection.on('close', () => {
+      console.log('WebSocket connection closed');
+
       const connectionIdx = connections.indexOf(connection);
       connections.splice(connectionIdx, 1);
     });

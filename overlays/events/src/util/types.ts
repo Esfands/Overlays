211e1ee030d5a180d3508654d3aba34b79cf382a
Adam Davies <acdavies001@gmail.com>
2022-11-22 01:43:17 -0600

    Move overlays

--- overlays/events/src/util/types.ts
+++
@@ -1,7 +0,0 @@
-export type WebSocketHook = [EventData, boolean];
-
-export type EventData = Record<any, any> | null;
-export type EventDates = {
-  started: Date;
-  ends: Date;
-};
---
+++ src/overlays/events/src/util/types.ts
@@ -0,0 +1,5 @@
+export type OverlayOffset = number | 'top' | 'middle' | 'bottom';
+export type EventDates = {
+  started: Date;
+  ends: Date;
+};

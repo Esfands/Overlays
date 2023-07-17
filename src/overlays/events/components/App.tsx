e2da9b1c455e87241a4490ce16e438e812f9772f
Adam Davies <acdavies001@gmail.com>
2023-07-16 19:07:22 -0500

    Improve events base

--- src/overlays/events/components/App.tsx
+++ src/overlays/events/components/App.tsx
@@ -2,23 +2,21 @@ import classNames from 'classnames';
 import Prediction from './prediction/Prediction';
 import Poll from './poll/Poll';
 import Marquee from './marquee/Marquee';
-import type { MultiEventData,  } from 'src/util/types';
-import type { OverlayOffset } from '../util/types';
+import type { MessageState } from 'src/util/types';
 
-type Props = Pick<MultiEventData, 'poll' | 'prediction'>;
-
-const offsetMap: Partial<Record<OverlayOffset, number>> = {
-  top: 0,
-  middle: 300,
+type Props = {
+  message: MessageState;
 };
 
-const App = ({ poll, prediction }: Props) => {
-  let offset: OverlayOffset = prediction?.offset || poll?.offset;
-  offset = isNaN(Number(offset)) ? offsetMap[offset] || 0 : offset;
+const App = ({ message }: Props) => {
+  const isPoll = message?.topic.includes('poll');
+  const isPrediction = message?.topic.includes('prediction');
+
+  const offset = 'top';
 
   const eventListClasses = classNames({
-    'no-event': !prediction && !poll,
-    'position-absolute': offset > 0,
+    'no-event': !isPoll && !isPrediction,
+    'position-absolute': typeof offset === 'number' && offset > 0,
   });
 
   const eventListStyle = {
@@ -27,10 +25,10 @@ const App = ({ poll, prediction }: Props) => {
 
   return (
     <div className="d-flex flex-column justify-content-end align-items-end">
-      <Marquee data={prediction} />
+      <Marquee message={message} />
       <div id="events" className={eventListClasses} style={eventListStyle}>
-        <Prediction data={prediction} />
-        <Poll data={poll} />
+        <Prediction message={message} />
+        <Poll message={message} />
       </div>
     </div>
   );

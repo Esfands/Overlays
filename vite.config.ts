a5d4528b3da615c7948b3027c668334f47620fa2
Adam Davies <acdavies001@gmail.com>
2023-07-16 22:45:37 -0500

    Fix import alias in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+  },
 });

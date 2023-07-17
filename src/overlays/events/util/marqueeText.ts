27776a1eb0d66a2b31c63f63c5060ff38e4f10aa
Adam Davies <acdavies001@gmail.com>
2023-07-16 19:28:27 -0500

    Improve marquee text

--- src/overlays/events/util/marqueeText.ts
+++ src/overlays/events/util/marqueeText.ts
@@ -1,31 +1,42 @@
-/* eslint-disable prettier/prettier */
-
 import { formatCurrency } from './formatters';
 
-const getRandomElement = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
+const winnerTemplates = [
+  'Luck hits "{name}" with a massive {amount} Ret Coin win.',
+  'Twitch local "{name}" wins {amount} Ret Coin at the tables.',
+  'Chatter "{name}" scrapes up {amount} Ret Coin at the tables.',
+  'Chatter "{name}" leads the believers with {amount} Ret Coin in total winnings.',
+];
+
+const loserTemplates = [
+  'Shameless: Twitch user "{name}" burns {amount} Ret Coin on unlikely bet.',
+  'Shameless gambler "{name}" sells their house to throw away {amount} Ret Coin.',
+  'Gambling addict "{name}" absolutely blows it with a loss of {amount} Ret Coin.',
+  'Shameless bettor "{name}" forks over {amount} Ret Coin for a wildly misplaced gamble.',
+];
+
+const refundTemplates = [
+  'Up and coming Twitch streamer Esfand "Sukhbeer" TV admits to scamming viewers out of {total} Ret Coin. Refunds to follow shortly.',
+  'Local Iranian male (5\'4", long black hair) last seen robbing a bank with {total} Ret Coin. Likely wearing a black t-shirt and red shorts.',
+  'Short, dark-haired male closely resembling Lord Farquad from the "Shrek" movie series seen quietly stealing {total} Ret Coin from Twitch viewers.',
+];
+
+const getRandomText = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
 
 export const getOutcomeText = (
   winnerName: string,
-  winnerAmt: number,
+  winnerAmount: number,
   loserName: string,
-  loserAmt: number
+  loserAmount: number,
 ) => {
-  const formattedWinnerAmt = formatCurrency(winnerAmt);
-  const formattedLoserAmt = formatCurrency(loserAmt);
+  const formattedWinnerAmount = formatCurrency(winnerAmount);
+  const formattedLoserAmount = formatCurrency(loserAmount);
 
-  const winnerText = getRandomElement([
-    `Luck hits "${winnerName}" with a massive ${formattedWinnerAmt} Ret Coin win.`,
-    `Twitch local "${winnerName}" wins ${formattedWinnerAmt} Ret Coin at the tables.`,
-    `Chatter "${winnerName}" scrapes up ${formattedWinnerAmt} Ret Coin at the tables.`,
-    `Chatter "${winnerName}" leads the believers with ${formattedWinnerAmt} Ret Coin in total winnings.`,
-  ]);
-
-  const loserText = getRandomElement([
-    `Shameless: Twitch user "${loserName}" burns ${formattedLoserAmt} Ret Coin on unlikely bet.`,
-    `Shameless gambler "${loserName}" sells their house to throw away ${formattedLoserAmt} Ret Coin.`,
-    `Gambling addict "${loserName}" absolutely blows it with a loss of ${formattedLoserAmt} Ret Coin.`,
-    `Shameless bettor "${loserName}" forks over ${formattedLoserAmt} Ret Coin for a wildly misplaced gamble.`,
-  ]);
+  const winnerText = getRandomText(winnerTemplates)
+    .replace(/\{name\}/g, winnerName)
+    .replace(/\{amount\}/g, formattedWinnerAmount);
+  const loserText = getRandomText(loserTemplates)
+    .replace(/\{name\}/g, loserName)
+    .replace(/\{amount\}/g, formattedLoserAmount);
 
   return `${winnerText} ${loserText}`;
 };
@@ -33,9 +44,5 @@ export const getOutcomeText = (
 export const getRefundText = (total: number) => {
   const formattedTotal = formatCurrency(total);
 
-  return getRandomElement([
-    `Up and coming Twitch streamer Esfand "Sukhbeer" TV admits to scamming viewers out of ${formattedTotal} Ret Coin. Refunds to follow shortly.`,
-    `Local Iranian male (5'6", long black hair) last seen robbing a bank with ${formattedTotal} Ret Coin. Likely wearing a black t-shirt and red shorts.`,
-    `Short, dark-haired male closely resembling Lord Farquad from the movie series Shrek seen quiety stealing ${formattedTotal} Ret Coin from Twitch viewers.`,
-  ]);
+  return getRandomText(refundTemplates).replace(/\{total\}/g, formattedTotal);
 };

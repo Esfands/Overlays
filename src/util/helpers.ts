0cad7eeddc3e8837adc8d2a521a2f3078905f718
Adam Davies <acdavies001@gmail.com>
2023-07-20 19:56:31 -0500

    Update message hook

--- src/util/helpers.ts
+++
@@ -1,9 +0,0 @@
-import type { MessageCode, WebSocketMessage } from './types';
-
-export function buildMessage(code: MessageCode, data: any): WebSocketMessage {
-  return {
-    d: data,
-    mc: code,
-    t: Date.now(),
-  };
-}
--- src/util/useMessage.ts
+++ src/util/useMessage.ts
@@ -2,18 +2,7 @@ import { useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { useDispatch } from '@/state/store';
 import { setMessage } from '@/state/slices';
-import { buildMessage } from './helpers';
-import { MessageCode } from './types';
-
-const TOPICS = [
-  'channel.poll.begin',
-  'channel.poll.progress',
-  'channel.poll.end',
-  'channel.prediction.begin',
-  'channel.prediction.progress',
-  'channel.prediction.lock',
-  'channel.prediction.end',
-];
+import { MessageCode, type WebSocketMessage } from '@server/types';
 
 const WEBSOCKET_URL = import.meta.env.VITE_WEBSOCKET_URL;
 const websocketOptions = {
@@ -22,33 +11,27 @@ const websocketOptions = {
 
 export default function useMessage(): boolean {
   const dispatch = useDispatch();
-  const { sendMessage, lastMessage, readyState } = useWebSocket(
-    WEBSOCKET_URL,
-    websocketOptions,
-  );
+  const { lastMessage, readyState } = useWebSocket(WEBSOCKET_URL, websocketOptions);
 
   useEffect(() => {
     if (readyState === ReadyState.OPEN) {
       console.log('WebSocket connection opened');
-      const newMessage = buildMessage(MessageCode.SUBSCRIBE, { topics: TOPICS });
-      sendMessage(JSON.stringify(newMessage));
-      return;
     }
 
     if (readyState === ReadyState.CLOSED) {
       console.log('WebSocket connection closed');
-      dispatch(setMessage(null));
+      dispatch(setMessage({}));
       return;
     }
   }, [readyState]);
 
   useEffect(() => {
     if (lastMessage) {
-      const { d: data, mc: code } = JSON.parse(lastMessage.data);
+      const { mc, d }: WebSocketMessage = JSON.parse(lastMessage.data);
 
-      if (code === MessageCode.DISPATCH) {
-        console.log(`[${data.topic}]`, data.payload);
-        dispatch(data);
+      if (mc === MessageCode.DISPATCH && typeof d !== 'string') {
+        console.log(`NEW MESSAGE [${d.event}]`, d);
+        dispatch(setMessage(d));
       }
     }
   }, [lastMessage]);
